18915d49ce9d59f03c841e79f262a6766c7519b4
Yogesh Sharma <yogesh.sharma01@nagarro.com>
2021-09-15 19:29:37 +0530

    Initial commit

---
+++ src/test/java/com/jupiter/ms/app/gateway/CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jupiter.ms.app.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
